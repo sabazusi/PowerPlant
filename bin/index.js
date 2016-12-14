f16ab277d8066003738d9564e65193d61c9ae22b
sabazusi <kouhei.live@gmail.com>
2016-12-15 01:15:03 +0900

    Add project name question

--- bin/index.js
+++ bin/index.js
@@ -1,9 +1,14 @@
 var inquirer = require('inquirer');
 var questions = [
   {
+    type: 'input',
+    name: 'input project name',
+    message: 'Input project name'
+  },
+  {
     type: 'confirm',
     name: 'createStandard',
     message: 'Create standard electron project'
   }
 ];
-inquirer.prompt(questions);
+inquirer.prompt(questions).then((answer) => console.log(answer));
