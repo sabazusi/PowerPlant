effb030df8d731039a84248e60aa34456dfd71f2
sabazusi <kouhei.live@gmail.com>
2016-12-16 01:39:07 +0900

    Add project type question, and project name validator

--- bin/index.js
+++ bin/index.js
@@ -1,14 +1,28 @@
-var inquirer = require('inquirer');
-var questions = [
+const inquirer = require('inquirer');
+
+const projectNameValidater = (name) => {
+  if (!name) return 'Input something..';
+  return true;
+};
+const projectQuestions = [
+  {
+    type: 'list',
+    name: 'projectType',
+    message: 'Select electron project type',
+    choices: ['standard']
+  },
   {
     type: 'input',
-    name: 'input project name',
-    message: 'Input project name'
-  },
+    name: 'projectName',
+    message: 'Input project name',
+    validate: projectNameValidater
+  }
+];
+const confirmQuestion = [
   {
     type: 'confirm',
     name: 'createStandard',
     message: 'Create standard electron project'
   }
 ];
-inquirer.prompt(questions).then((answer) => console.log(answer));
+inquirer.prompt(projectQuestions).then((answer) => console.log(answer));
