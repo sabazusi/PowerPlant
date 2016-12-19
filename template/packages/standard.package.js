afcc2ccade7c2ec0303015ab6542deabd753a979
sabazusi <kouhei.live@gmail.com>
2016-12-20 01:53:18 +0900

    Change packages

---
+++ template/packages/standard.package.js
@@ -0,0 +1,13 @@
+module.exports = {
+  dependencies: [
+  ],
+  devDependencies: [
+    'babel-core',
+    'babel-loader',
+    'babel-preset-es2015',
+    'electron',
+    'electron-packager',
+    'webpack',
+    'webpack-merge'
+  ]
+};
